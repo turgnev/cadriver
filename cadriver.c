2a6b068ab9ed88024a53a45eb29c2ee752ba42a3
Vladislav Turgenev <vladislav_turgen@mail.ru>
2018-11-06 02:38:26 +0300

    interrupt request added

--- cadriver.c
+++ cadriver.c
@@ -5,16 +5,44 @@
 MODULE_LICENSE("GPL");      
 MODULE_AUTHOR("Vladislav Turgenev");  
 MODULE_DESCRIPTION("Pulse coin acceptor driver"); 
-MODULE_VERSION("0.1");       
+MODULE_VERSION("0.1");     
 
-static int __init cadriver_init(void) {
-   printk(KERN_INFO "Coin acceptor module is loaded\n");
-   return 0;
+static int ca_counter = 0;
+
+static int irq_number; 
+static unsigned int signal_pin = 18;    // pin 12 
+
+static irq_handler_t signal_pin_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
+
+static int __init cadriver_init(void) 
+{
+    printk(KERN_INFO "Coin acceptor module is loaded\n");
+
+    gpio_request(signal_pin, "sysfs");       
+    gpio_direction_input(signal_pin);        
+    gpio_export(signal_pin, false);          
+
+
+    irq_number = gpio_to_irq(gpioButton);     
+    printk(KERN_INFO "%d pin mapped to IRQ: %d\n",signal_pin, irq_number);
+
+    int result = 0;
+    result = request_irq(irq_number, (irq_handler_t) signal_pin_irq_handler, IRQF_TRIGGER_RISING,  "my_interrupt",  NULL); 
+    return result;
 }
 
-static void __exit cadriver_exit(void) {
-   printk(KERN_INFO "Coin acceptor module is unloaded\n");
+static void __exit cadriver_exit(void) 
+{
+    printk(KERN_INFO "Coin acceptor module is unloaded\n");
 }
+
+static irq_handler_t signal_pin_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs)
+{
+    printk(KERN_INFO "Coin accepted\n");
+    ca_counter++;                    
+    return (irq_handler_t) IRQ_HANDLED; 
+}
+
 
 module_init(cadriver_init);
 module_exit(cadriver_exit);
